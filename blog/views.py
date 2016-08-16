2c5d6f71ac69b32d8f450282884e2e7eb7fd611c
peterolayinka <peterolayinka97@gmail.com>
2016-08-16 09:07:30 +0100

    a little modification like comment cont

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .forms import PostForm, CommentForm
 
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+
 from .models import Post, Comment
 
 # Create your views here.
@@ -25,13 +28,14 @@ def post_detail(request, post_id):
 
 	return render(request, 'blog/post_detail.html', {'post_data': post_data, 'form': form} )
 
+@login_required
 def post_new(request):
 	if request.method == 'POST':
 		form = PostForm(request.POST)
 		if form.is_valid():
 			post = form.save(commit=False)
 			post.author = request.user
-			post.published_date = timezone.now()
+			# post.published_date = timezone.now()
 			post.save()
 			return redirect('post_detail', post_id=post.pk)
 	else:
@@ -39,6 +43,7 @@ def post_new(request):
 
 	return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
 def post_edit(request, post_id):
 	post = get_object_or_404(Post, pk=post_id)
 	if request.method == 'POST':
@@ -53,3 +58,23 @@ def post_edit(request, post_id):
 
 	return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
+def post_draft_list(request):
+	posts = Post.objects.filter(published_date__isnull=True).order_by('-created_date')
+	return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+@login_required
+def post_publish(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	post.publish()
+	return redirect('post_detail', post_id=pk)
+
+@login_required
+def post_remove(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	post.delete()
+	return redirect('post_list')
+
+def logout(request):
+	logout(request)
+	return redirect('post_list')
